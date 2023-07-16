eb4a6f485be9fa701414e21addbd76049788a3ad
Milos <srejic.milos94@outlook.com>
2023-07-16 15:50:44 +0200

    Changed most of the script so it works properly

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-// All the options that computer or user can pick
+// Options that computer or player can pick
 const choices = ["rock", "paper", "scissors"];
 
 // Getting computer choice from the choices array
 function getComputerChoice() {
-    // Generating random index number for an array called choices
-    let computerChoiceIndex = Math.floor(Math.random() * choices.length);
-
-    // computerChoice gets assigned a random value from an array called choices
-    let computerChoice = choices[computerChoiceIndex];
-
-    // Just returning the value of computerChoice
+    let randomIndex = Math.floor((Math.random() * choices.length)); //Generating random index
+    let computerChoice = choices[randomIndex]; //ComputerChoice gets assigned a random value from an array choices
     return computerChoice;
 }
 
+// Getting user choice from the prompt
+function getUserChoice() {
+    let validatedInput = false;
+        while(validatedInput == false) {
+            const userChoice = prompt("Pick your choice: rock, paper or scissors?");
+            if (userChoice == null) {
+                continue;
+            }
 
-// Function that determinates the winner
+        const userChoiceLowCase = userChoice.toLowerCase();
+            if(choices.includes(userChoiceLowCase)) {
+                validatedInput = true;
+                return userChoiceLowCase;
+            }
+        }
+}
+
+
+// Function that determinates the winner of the round
 function isWinner(userSelection, computerSelection) {
     if (userSelection === computerSelection) {
-        return "Tie!";
+        return "Tie";
     }
     else if ((userSelection === "rock" && computerSelection === "scissors") ||
         (userSelection === "paper" && computerSelection === "rock") ||
@@ -27,61 +39,60 @@ function isWinner(userSelection, computerSelection) {
     else return "Computer";
 }
 
-// Running the round
-function run () {
-// Function which starts the round and returns winner as a result
+
+
+// Returning round score
 function playRound(userSelection, computerSelection) {
-    let result = isWinner(userSelection, computerSelection);
-    if (result === "Tie!") {
+    const roundWinner = isWinner(userSelection, computerSelection);
+    if (roundWinner == "Tie") {
         return "It is a tie!";
-    } else if (result === "Player") {
-        return "Player wins!" 
-    } else if (result === "Computer") {
-        return "Computer wins!"
-    }
+    } else if (roundWinner == "Player") {
+        return "Player wins!";
+    } else return "Computer wins!";
 }
 
 
-// userselection value is the value of the prompt, converted to lower case
-let userSelection = prompt("What is your choice?").toLowerCase();
 
-// computer selection value is the value from the function getComputerChoice(random value from the array)
-let computerSelection = getComputerChoice();
-
-// Logging results in the console
-function resultLog() {
-    // If userSelection is anything other than the values in the choices array
-    if (!choices.includes(userSelection)) {
-        console.log("Invalid input! Please refresh the page and enter a valid option!")
-    }
-    // If userSelection is a valid choice
-    else {
-    console.log(playRound(userSelection, computerSelection));
-    console.log("User's choice:", userSelection);
-    console.log("Computer's choice:", computerSelection);
-    }
-}
-resultLog();
-}
-
-
-// Function that runs 5 rounds of the game Rock Paper Scissors and declares a final winner
+// Function that plays 5 rounds of the game and shows who is a winner
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+    let userScore = 0; // player score
+    let computerScore = 0; // computer score
 
-    for (let i = 0; i < 5; i++) {
-        run();
-        if (isWinner === "Player") {
-            playerScore++;
-        } else if (isWinner === "Computer") {
+    // Looping through the 5 rounds
+    for (let round = 1; round <= 5; round++) {
+        let userSelection = getUserChoice(); //userSelection has the value that function getUserChoice() returns
+        let computerSelection = getComputerChoice(); //computerSelection has the value that function getComputerChoice returns
+
+        // Logging number of rounds within the console
+        console.log(`Round ${round}`);
+
+        // Running playRound function for 5 times
+        playRound(userSelection, computerSelection);
+
+        // Increasing the scores through conditions
+        if (isWinner(userSelection, computerSelection) == "Player") {
+            userScore++;
+        } else if (isWinner(userSelection, computerSelection) == "Computer") {
             computerScore++;
-        } else playerScore, computerScore;
-    }
-    
-    playerScore > computerScore ? console.log("Congratulations! Player is a winner of the game!") : console.log("Better luck next time! Computer is a winner of the game!");
-};
+        };
 
-// Calling the game function
+        // Logging choices and round results within the console
+        console.log("User's choice:", userSelection);
+        console.log("Computer's choice:", computerSelection);
+        console.log(playRound(userSelection, computerSelection));
+}
+
+    // Logging final result depending on both player and computer score
+    console.log(`Player: ${userScore}, Computer: ${computerScore}`);
+    if (userScore < computerScore) {
+        console.log("Better luck next time! Computer wins the game!")
+    } else if (userScore > computerScore) {
+        console.log("Congratulations! Player wins!");
+    } else {
+        console.log("It is a draw!")
+    }
+}
+
+// Running the game
 game();
 
